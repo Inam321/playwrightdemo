7e409d31d3d5abf232b202cbd6b26407f4eaea05
Inam Rehman <Inam.Rehman@intechww.com>
2024-08-15 01:26:31 +0500

    first commit

--- tests/ab c.spec.js
+++ tests/ab c.spec.js
@@ -8,5 +8,4 @@ test('User Created & Delete Test', async ({ page }) => {
  await page.getByPlaceholder('Enter Password').fill('@#Intech123');
  await page.getByRole('button', { name: 'Login' }).click();
  await page.reload()
-
 });
