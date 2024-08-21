bc25850002ea23d52fde32b73f98381d8a00ff8c
Inam Rehman <Inam.Rehman@intechww.com>
2024-08-21 16:08:54 +0500

    first commit

--- tests/PDF EXCEL.spec.js
+++ tests/PDF EXCEL.spec.js
@@ -7,7 +7,7 @@ test('REPORT01', async ({ page }) => {
   // await CommonMethodsClass.removedata(page);
   // await CommonMethodsClass.deletehistorydata(page); //// if we need  histrical trends then disable this row
   await page.goto('https://stg.empowergpt.ai/chat');
-  await page.goto('https://www.google.com/');
+  //await page.goto('https://www.google.com/');
   await page.getByPlaceholder('Enter Email').fill('Admin');
   await page.getByPlaceholder('Enter Password').click();
   await page.getByPlaceholder('Enter Password').fill('@#Intech123');
