57a70233dfbf0c9eb8a124aba6f5bfbadbc882b1
Inam Rehman <Inam.Rehman@intechww.com>
2024-08-21 15:48:58 +0500

    first commit

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-npx playwright test .\tests\User CDER.spec.js --project=chromium headed & npx playwright test .\tests\Roles CEDR.spec.js --project=firefox headed
+npx playwright test .\tests\Roles CEDR.spec.js --project=chromium headed & npx playwright test .\tests\Roles CEDR.spec.js --project=firefox headed
 
