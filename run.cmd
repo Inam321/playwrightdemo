0f194214c455cb4691a9a4434feee114379fccc5
Inam Rehman <Inam.Rehman@intechww.com>
2024-08-21 15:45:17 +0500

    first commit

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-npx playwright test .\tests\PDF EXCEL.spec.js --project=chromium headed & allure generate allure-results -o allure-report --clean & npx playwright test .\tests\small f.spec.js --project=chromium headed & allure generate --single-file allure-results --clean & npx playwright test .\tests\email g.spec.js --project=firefox headed
+npx playwright test .\tests\User CDER.spec.js --project=chromium headed & npx playwright test .\tests\Roles CEDR.spec.js --project=firefox headed
 
